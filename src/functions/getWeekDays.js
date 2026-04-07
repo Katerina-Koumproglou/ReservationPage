71315523fdfd8775cf46c352c0c06734c0188a04
Katerina Koumproglou <kat.koumproglou@gmail.com>
2026-04-07 19:28:37 +0200

    changed icon and tab title, also moved App.js contents to ReservationPage.js

--- src/functions/getWeekDays.js
+++ src/functions/getWeekDays.js
@@ -2,7 +2,7 @@
 export const getWeekDays = (date) => {
   const start = new Date(date);
   //Move the day back to Sunday to get the first day of the week
-  start.setDate(start.getDate() - start.getDay()); //4
+  start.setDate(start.getDate() - start.getDay());
 
   return Array.from({ length: 7 }).map((_, i) => {
     const day = new Date(start);
---
+++ src/pages/ReservationPage.js
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import "../App.css";
+import "react-datepicker/dist/react-datepicker.css";
+import notifSound from "../assets/sounds/notif-sound.mp3";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import ContactInfo from "../components/UILayout/ContactInfo";
+import ReservationDetails from "../components/UILayout/ReservationDetails";
+import validateUserInfo from "../functions/validateNecessaryFields";
+
+import { SubmitButton } from "../styles/Buttons";
+import { ThemeProvider } from "styled-components";
+import { theme } from "../styles/theme";
+import { SectionWrapper } from "../styles/Wrappers";
+const ReservationPage = () => {
+  const initialState = {
+    name: "",
+    email: "",
+    phoneNumber: "",
+    reservationDate: new Date(),
+    selectedSlot: null,
+  };
+
+  const [userInfo, setUserInfo] = useState(initialState);
+
+  function handleChange(e) {
+    const { id, value } = e.target;
+    setUserInfo((prev) => ({ ...prev, [id]: value }));
+  }
+
+  // Handle submit button
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    //Handle errors
+    const tempErrors = validateUserInfo(userInfo);
+    setErrors(tempErrors);
+    if (Object.keys(tempErrors).length !== 0) return;
+
+    //Reservation submitted successfully
+    console.log("Reservation sumbitted for: ", userInfo);
+    const sound = new Audio(notifSound);
+    sound.play();
+    toast.success("Reservation submitted successfully!");
+    // setTimeout(() => {
+    //   alert("Reservation submitted successfully!");
+    // }, 1000);
+    setUserInfo(initialState);
+    setErrors({});
+  }
+
+  //Handle errors
+  const [errors, setErrors] = useState({});
+
+  return (
+    <ThemeProvider theme={theme}>
+      <form className="App" onSubmit={handleSubmit}>
+        <SectionWrapper>
+          {/* LEFT COLUMN */}
+          <ContactInfo
+            userInfo={userInfo}
+            handleChange={handleChange}
+            errors={errors}
+          />
+
+          {/* RIGHT COLUMN */}
+          <ReservationDetails
+            userInfo={userInfo}
+            handleChange={handleChange}
+            errors={errors}
+          />
+        </SectionWrapper>
+        {/* SUBMIT BUTTON */}
+        <SubmitButton>RESERVE APPOINTMENT</SubmitButton>
+        <ToastContainer />
+      </form>
+    </ThemeProvider>
+  );
+};
+
+export default ReservationPage;
